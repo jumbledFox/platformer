7697c03fb28dc14ec7e0eeb89c9e43bc93f8d680
jumbledFox <81022536+jumbledFox@users.noreply.github.com>
2024-06-26 10:14:44 +0100

    work on physics

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use macroquad::{camera::{set_camera, Camera2D}, color::{Color, WHITE}, input::is_key_pressed, math::{vec2, Rect}, texture::{draw_texture_ex, DrawTextureParams, Texture2D}, time::get_frame_time, window::{clear_background, next_frame, screen_height, screen_width, Conf}};
+use macroquad::{camera::{set_camera, Camera2D}, color::{Color, WHITE}, input::{is_key_down, is_key_pressed}, math::{vec2, Rect}, texture::{draw_texture_ex, DrawTextureParams, Texture2D}, time::get_frame_time, window::{clear_background, next_frame, screen_height, screen_width, Conf}};
 use player::Player;
 
 pub mod player;
@@ -18,23 +18,26 @@ async fn main() {
     let tiles_texture = Texture2D::from_file_with_format(include_bytes!("tiles.png"), None);
     tiles_texture.set_filter(macroquad::texture::FilterMode::Nearest);
 
-    let mut p = Player::new(vec2(32.0, 32.0));
+    let mut p = Player::new(vec2(2.0, 5.0) * 16.0);
     let mut helmet = false;
+
+    let mut recip_mul = 1.0;
 
     loop {
         let delta = get_frame_time();
-        let sprite = p.update(delta);
+        let sprite = p.update(delta, recip_mul);
         if is_key_pressed(macroquad::input::KeyCode::Q) { helmet = !helmet; }
 
+        // if is_key_down(macroquad::input::KeyCode::A)
 
         let screen_size = vec2(screen_width(), screen_height());
-        let scale = 5.0;
-        let view_area = screen_size / (scale / 2.0);
+        let scale = 2.0;
+        let view_area = screen_size / scale;
         
         clear_background(Color::from_hex(0x6dcaff));
         // clear_background(BLACK);
         set_camera(&Camera2D {
-            zoom: scale / screen_size,
+            zoom: (scale * 2.0) / screen_size,
             target: view_area / 2.0,
             ..Default::default()
         });
@@ -69,7 +72,8 @@ async fn main() {
                 });
             }
         }
-        draw_texture_ex(&player_texture, p.pos().x, view_area.y - p.pos().y - 32.0, WHITE, DrawTextureParams {
+        
+        draw_texture_ex(&player_texture, p.pos().x.round(), (view_area.y - p.pos().y - 32.0).round(), WHITE, DrawTextureParams {
             flip_x: p.flip_x(),
             source: Some(Rect::new(sprite as f32 * 16.0, if helmet { 32.0 } else { 0.0 }, 16.0, 32.0)),
             ..Default::default()
--- src/player.rs
+++ src/player.rs
@@ -1,8 +1,16 @@
 use macroquad::{input::{is_key_down, is_key_pressed, KeyCode}, math::{FloatExt, Vec2}};
 
+
+const KEY_MOVE_LEFT:  KeyCode = KeyCode::Left;
+const KEY_MOVE_RIGHT: KeyCode = KeyCode::Right;
+const KEY_JUMP:       KeyCode = KeyCode::Z;
+const KEY_RUN:        KeyCode = KeyCode::A;
+
+#[derive(Default)]
 pub struct Player {
     flip_x: bool,
     jumping: bool,
+    walk_timer: f32,
 
     pos: Vec2,
     vel: Vec2,
@@ -10,8 +18,9 @@ pub struct Player {
     gravity: f32,
     
     // Constants
-    walk_speed: f32,
-    run_speed:  f32,
+    walk_speed:   f32,
+    run_speed:    f32,
+    jump_height:  f32,
     jump_gravity: f32,
     fall_gravity: f32,
 }
@@ -21,53 +30,64 @@ impl Player {
         Player {
             flip_x: false,
             jumping: false,
+            walk_timer: 0.0,
 
             pos,
-            vel: Vec2::ZERO,
-            target_x_vel: 0.0,
-            gravity: 0.0,
 
-            walk_speed: 100.0,
-            run_speed:  100.0,
-            jump_gravity: 256.0,
-            fall_gravity: 512.0,
+            walk_speed:     70.0,
+            run_speed:     110.0,
+            jump_height:   250.0,
+            jump_gravity:  512.0,
+            fall_gravity: 1024.0,
+
+            ..Default::default()
         }
     }
 
     pub fn pos(&self)    -> Vec2 { self.pos }
     pub fn flip_x(&self) -> bool { self.flip_x }
 
-    pub fn update(&mut self, delta: f32) -> usize {
+    pub fn update(&mut self, delta: f32, recip_mul: f32) -> usize {
         let grounded = self.pos.y <= 16.0;
 
         // Jumping
-        self.vel.y -= self.gravity * delta;
-
-        if is_key_down(KeyCode::Space) && grounded {
-            self.vel.y = 150.0; 
+        if is_key_pressed(KEY_JUMP) && grounded {
+            self.vel.y = self.jump_height; 
             self.jumping = true;
         }
 
-        let target_gravity = match is_key_down(KeyCode::Space) && self.jumping {
+        if !is_key_down(KEY_JUMP) {
+            self.jumping = false;
+        }
+        let target_gravity = match is_key_down(KEY_JUMP) && self.jumping {
             true  => self.jump_gravity,
             false => self.fall_gravity,
-        };
-
+        } * (self.vel.x.recip() * recip_mul).abs().clamp(0.5, 1.0);
         self.gravity = self.gravity.lerp(target_gravity, delta * 100.0);
+        self.vel.y -= self.gravity * delta;
 
-        println!("{:?}", self.gravity);
+        // println!("{:?}", self.gravity);
 
         // Moving
-        if is_key_pressed(KeyCode::A) { self.target_x_vel = -self.walk_speed; self.flip_x = true;  }
-        if is_key_pressed(KeyCode::D) { self.target_x_vel =  self.walk_speed; self.flip_x = false; }
-        if !is_key_down(KeyCode::A) && !is_key_down(KeyCode::D) { self.target_x_vel = 0.0; }
+        let target_speed = match is_key_down(KEY_RUN) {
+            false => self.walk_speed,
+            true  => self.run_speed,
+        };
 
-        // self.vel.x = match self.target_x_vel self.vel.x.lerp(self.target_x_vel, delta * 5.0);
+        if is_key_pressed(KEY_MOVE_LEFT)  { self.target_x_vel = -target_speed; self.flip_x = true;  }
+        if is_key_pressed(KEY_MOVE_RIGHT) { self.target_x_vel =  target_speed; self.flip_x = false; }
+        if !is_key_down(KEY_MOVE_LEFT) && !is_key_down(KEY_MOVE_RIGHT) && grounded { self.target_x_vel = 0.0; }
 
-        self.vel.x = self.vel.x.to_target(self.target_x_vel, delta * 400.0);
+        self.vel.x = self.vel.x.to_target(self.target_x_vel, delta * 400.0 * if grounded { 1.0 } else { 2.0 });
 
-        println!("---\n{:?}\n{:?}\n", self.vel.x, self.target_x_vel);
+        self.walk_timer = (self.walk_timer + self.vel.x.abs() / 1000.0).rem_euclid(1.0);
 
+        if self.vel.x == 0.0 { self.walk_timer = 0.0; }
+
+        // println!("{:?}", self.vel.x);
+
+        // println!("---\n{:?}\n{:?}\n", self.vel.x, self.target_x_vel);
+        
         self.pos += self.vel * delta;
 
         let grounded = self.grounded();
@@ -78,11 +98,11 @@ impl Player {
         
         match (self.vel.x.abs() == 0.0, self.vel.x.abs() > 75.0, grounded, self.vel.y > -50.0) {
             (true, _,  true, _)  => 0,
-            (_, false, true, _)  => 2, 
-            (_, true,  true, _)  => 3, 
+            (_, false, true, _)  => if self.walk_timer >= 0.5 { 0 } else { 2 }, 
+            (_, true,  true, _)  => if self.walk_timer >= 0.5 { 1 } else { 3 }, 
             (_, _, false, true)  => 4,
             (_, _, false, false) => 5,
-        } 
+        }
     }
 
     fn grounded(&self) -> bool {
