a255f4839e9d5d87c138f1dc54ffa8a82c1f5c56
jumbledFox <81022536+jumbledFox@users.noreply.github.com>
2024-06-28 19:26:29 +0100

    do some great work!! :3

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,74 @@
-use macroquad::{camera::{set_camera, Camera2D}, color::{Color, WHITE}, input::{is_key_down, is_key_pressed}, math::{vec2, Rect}, texture::{draw_texture_ex, DrawTextureParams, Texture2D}, time::get_frame_time, window::{clear_background, next_frame, screen_height, screen_width, Conf}};
+use macroquad::{camera::{set_camera, Camera2D}, color::{Color, WHITE}, input::is_key_pressed, math::{vec2, Rect}, texture::{draw_texture_ex, DrawTextureParams, Texture2D}, time::get_frame_time, window::{clear_background, next_frame, screen_height, screen_width, Conf}};
 use player::Player;
 
 pub mod player;
 
+const SCALE: i32 = 6;
+const SCALE_F: f32 = SCALE as f32;
+
 fn window_conf() -> Conf {
     Conf {
         window_title: String::from("Platformer"),
-        high_dpi: false,
+        window_width:  352 * SCALE,
+        window_height: 224 * SCALE,
+        high_dpi: true,
         ..Default::default()
     }
 }
 #[macroquad::main(window_conf())]
 
 async fn main() {
-    let player_texture = Texture2D::from_file_with_format(include_bytes!("sprites.png"), None);
+    let player_texture = Texture2D::from_file_with_format(include_bytes!("../res/sprites.png"), None);
     player_texture.set_filter(macroquad::texture::FilterMode::Nearest);
-    let tiles_texture = Texture2D::from_file_with_format(include_bytes!("tiles.png"), None);
+    let tiles_texture = Texture2D::from_file_with_format(include_bytes!("../res/tiles.png"), None);
     tiles_texture.set_filter(macroquad::texture::FilterMode::Nearest);
 
-    let mut p = Player::new(vec2(2.0, 5.0) * 16.0);
+    let mut p = Player::new(vec2(2.0, 1.0) * 16.0);
     let mut helmet = false;
-
-    let mut recip_mul = 1.0;
 
     loop {
         let delta = get_frame_time();
-        let sprite = p.update(delta, recip_mul);
+        let sprite = p.update(delta);
         if is_key_pressed(macroquad::input::KeyCode::Q) { helmet = !helmet; }
 
-        // if is_key_down(macroquad::input::KeyCode::A)
-
         let screen_size = vec2(screen_width(), screen_height());
-        let scale = 3.0;
-        let view_area = screen_size / scale;
+        let scale = SCALE_F;
+        let view_area = screen_size / scale * 2.0;
         
         clear_background(Color::from_hex(0x6dcaff));
         // clear_background(BLACK);
         set_camera(&Camera2D {
-            zoom: (scale * 2.0) / screen_size,
+            zoom: scale / screen_size,
             target: view_area / 2.0,
             ..Default::default()
         });
 
-
-        for i in 0..(view_area.x / 16.0).ceil() as usize {
-            draw_texture_ex(&tiles_texture, i as f32 * 16.0, view_area.y - 16.0, WHITE, DrawTextureParams {
-                source: Some(Rect::new(9.0 * 16.0, 0.0, 16.0, 16.0)),
+        let draw_tile = |x: f32, y: f32, t: f32| {
+            draw_texture_ex(&tiles_texture, x*16.0, y*16.0, WHITE, DrawTextureParams {
+                source: Some(Rect::new(t * 16.0, 0.0, 16.0, 16.0)),
                 ..Default::default()
             });
+        };
 
-            if i % 12 == 0 {
-                for j in 2..5 {
-                    draw_texture_ex(&tiles_texture, i as f32 * 16.0, view_area.y - 16.0 * j as f32, WHITE, DrawTextureParams {
-                        source: Some(Rect::new(6.0 * 16.0, 0.0, 16.0, 16.0)),
-                        ..Default::default()
-                    });
-                }
-            }
-            if i % 12 == 6 {
-                for j in 2..4 {
-                    draw_texture_ex(&tiles_texture, i as f32 * 16.0, view_area.y - 16.0 * j as f32, WHITE, DrawTextureParams {
-                        source: Some(Rect::new(6.0 * 16.0, 0.0, 16.0, 16.0)),
-                        ..Default::default()
-                    });
-                }
-            }
-            if i % 12 == 3 {
-                draw_texture_ex(&tiles_texture, i as f32 * 16.0, view_area.y - 16.0 * 4.0, WHITE, DrawTextureParams {
-                    source: Some(Rect::new(7.0 * 16.0, 0.0, 16.0, 16.0)),
-                    ..Default::default()
-                });
+        let bottom_y = (view_area.y / 16.0 - 1.0).ceil() * 16.0;
+        for i in 0..(view_area.x / 16.0).ceil() as usize {
+            let x = i as f32;
+            let y = bottom_y / 16.0;
+            draw_tile(x, y, 9.0);
+            match i % 12 {
+                0 => { for j in 1..4 { draw_tile(x, y - j as f32, 6.0); } },
+                6 => { for j in 1..3 { draw_tile(x, y - j as f32, 6.0); } },
+                3 => { draw_tile(x, y - 3.0 as f32,7.0); },
+                _=> {},
             }
         }
         
-        draw_texture_ex(&player_texture, p.pos().x.round(), (view_area.y - p.pos().y - 32.0).round(), WHITE, DrawTextureParams {
+        draw_texture_ex(&player_texture, p.pos().x.round(), (bottom_y - p.pos().y - 16.0).round(), WHITE, DrawTextureParams {
             flip_x: p.flip_x(),
             source: Some(Rect::new(sprite as f32 * 16.0, if helmet { 32.0 } else { 0.0 }, 16.0, 32.0)),
             ..Default::default()
         });
+
         next_frame().await
     }
 }
--- src/player.rs
+++ src/player.rs
@@ -11,10 +11,10 @@ pub struct Player {
     flip_x:   bool,
     jumping:  bool,
     skidding: bool,
-    skid_start_speed: f32,
     walk_timer: f32,
     walk_dir: Option<WalkDir>,
     running_time: f32, // How long you've been running for, used for the "p meter"
+    running: bool,
 
     pos: Vec2,
     vel: Vec2,
@@ -23,15 +23,10 @@ pub struct Player {
     
     // Constants
     walk_speed:    f32,
-
     run_beg_speed: f32,
-
-    run_mid_speed: f32,
-    run_mid_time:  f32,
-
     run_end_speed: f32,
-    run_end_time:  f32,
-
+    run_lerp_beg_time:  f32,
+    run_lerp_end_time:  f32,
     jump_height:   f32,
     jump_gravity:  f32,
     fall_gravity:  f32,
@@ -47,15 +42,14 @@ impl Player {
         Player {
             pos,
 
-            walk_speed:      70.0,
-            run_beg_speed:  110.0,
-            run_mid_speed:  120.0,
-            run_mid_time:     0.5,
-            run_end_speed:  140.0,
-            run_end_time:     1.2,
-            jump_height:    250.0,
-            jump_gravity:   512.0,
-            fall_gravity:  1024.0,
+            walk_speed:          70.0,
+            run_beg_speed:      110.0,
+            run_end_speed:      150.0,
+            run_lerp_beg_time:    0.4,
+            run_lerp_end_time:    1.2,
+            jump_height:        250.0,
+            jump_gravity:       512.0,
+            fall_gravity:      1024.0,
 
             ..Default::default()
         }
@@ -64,81 +58,93 @@ impl Player {
     pub fn pos(&self)    -> Vec2 { self.pos }
     pub fn flip_x(&self) -> bool { self.flip_x }
 
-    pub fn update(&mut self, delta: f32, _recip_mul: f32) -> usize {
+    pub fn update(&mut self, delta: f32) -> usize {
         let grounded = self.pos.y <= 16.0;
 
         // Jumping
-        if is_key_pressed(KEY_JUMP) && grounded {
-            self.vel.y = self.jump_height; 
-            self.jumping = true;
-        }
 
-        if !is_key_down(KEY_JUMP) {
-            self.jumping = false;
-        }
+        // if is_key_pressed(KEY_JUMP) && grounded {
+        //     self.vel.y = self.jump_height; 
+        //     self.jumping = true;
+        // }
 
-        let gravity_mul = (self.vel.x.recip() * 10.0).abs().clamp(0.0, 1.0);
-        let target_gravity = match is_key_down(KEY_JUMP) && self.jumping {
-            true  => self.jump_gravity,
-            false => self.fall_gravity,
-        } * gravity_mul;
-        self.gravity = self.gravity.lerp(target_gravity, delta * 100.0);
-        self.vel.y -= self.gravity * delta;
+        // if !is_key_down(KEY_JUMP) {
+        //     self.jumping = false;
+        // }
+
+        // let gravity_mul = (self.vel.x.recip() * 10.0).abs().clamp(0.0, 1.0);
+        // let target_gravity = match is_key_down(KEY_JUMP) && self.jumping {
+        //     true  => self.jump_gravity,
+        //     false => self.fall_gravity,
+        // } * gravity_mul;
+        // self.gravity = self.gravity.lerp(target_gravity, delta * 100.0);
+        // self.vel.y -= self.gravity * delta;
 
         // println!("{:?}", gravity_mul);
 
         // Moving
 
-        // Start moving if you press a direction
+        // Start moving if you press a direction.
         if is_key_pressed(KEY_MOVE_LEFT)  { self.walk_dir = Some(WalkDir::Left);  }
         if is_key_pressed(KEY_MOVE_RIGHT) { self.walk_dir = Some(WalkDir::Right); }
-        // Stop moving if you stop holding the direction you're going in
+        // Stop moving if you stop holding the direction you're going in.
         if (matches!(self.walk_dir, Some(WalkDir::Left))  && !is_key_down(KEY_MOVE_LEFT)
         ||  matches!(self.walk_dir, Some(WalkDir::Right)) && !is_key_down(KEY_MOVE_RIGHT)) && grounded {
             self.walk_dir = None;
         }
 
-        // If you're walking one way and actually going the other way, you should skid!
-        let prev_skidding = self.skidding;
-        let walking_opposing_velocity =
-           (self.walk_dir == Some(WalkDir::Left) && self.vel.x.is_sign_positive())
-        || (self.walk_dir == Some(WalkDir::Right) && self.vel.x.is_sign_negative());
-        self.skidding = grounded && walking_opposing_velocity;
-        
-        // If you started skidding this frame, make your x velocity lower so the skid is snappier (https://www.desmos.com/calculator/g18oxsmchz), and make sure you're not 'end' running
-        if !prev_skidding && self.skidding {
-            self.vel.x *= (0.4/-70.0) * self.vel.x.abs() + 1.3;
-            self.running_time = self.running_time.min(self.run_mid_time);
+        // If you're moving fast enough and holding a direction, you're running!
+        if self.vel.x.abs() >= self.run_beg_speed && self.walk_dir.is_some() {
+            self.running = true;
         }
-
+        // If you're not moving fast enough and not holding a direction, you shouldn't be running.
+        if self.vel.x.abs()  < self.run_beg_speed && self.walk_dir.is_none() {
+            self.running = false;
+        }
+        // If you're not holding down the run key, you're not running, duh!
+        if !is_key_down(KEY_RUN) {
+            self.running = false;
+        }
         // This is the timer for if you're running at maximum speed
-        // It should only be altered if you're grounded and running in a direction, increasing if you're running at the right speed or skidding
-        self.running_time += match (grounded, (self.vel.x.abs() >= self.run_beg_speed || self.skidding || self.walk_dir.is_some()) && is_key_down(KEY_RUN)) {
-            (false, _) => 0.0,
+        // If you're not grounded, it should freeze. Otherwise it should increase/decrease depending on if you're running or not.
+        self.running_time += match (!grounded, self.running) {
+            (true, _)  =>  0.0,
             (_, true)  =>  delta,
             (_, false) => -delta,
         };
-        self.running_time = self.running_time.clamp(0.0, self.run_end_time);
+        self.running_time = self.running_time.clamp(0.0, self.run_lerp_end_time);
 
-        let target_speed = match is_key_down(KEY_RUN) {
-            false    => self.walk_speed,
-            _ if self.running_time >= self.run_end_time => self.run_end_speed,
-            _ if self.running_time >= self.run_mid_time => self.run_mid_speed,
-            _                                           => self.run_beg_speed,
-        };
-        println!("{:?}", target_speed);
+        // If you're walking one way and actually going the other way, you should skid!
+        let prev_skidding = self.skidding;
+        let walking_opposing_velocity =
+           (self.walk_dir == Some(WalkDir::Left)  && self.vel.x.is_sign_positive() && self.vel.x != 0.0)
+        || (self.walk_dir == Some(WalkDir::Right) && self.vel.x.is_sign_negative() && self.vel.x != 0.0);
+        self.skidding = grounded && walking_opposing_velocity;
         
-        // let target_speed = match target_speed.abs() < self.skid_start_speed.abs() {
-        //     true  => target_speed.signum() * self.skid_start_speed,
-        //     false => target_speed,
-        // };
+        // If you started skidding this frame, make your x velocity lower so the skid is snappier (https://www.desmos.com/calculator/qmob28z1yb)
+        // And, if you're running fast, go back to the beginning running speed (by setting the running_time back).
+        if !prev_skidding && self.skidding {
+            let g = (1.0 - 0.5) / (self.run_beg_speed - self.run_end_speed);
+            self.vel.x *= (g*(self.vel.x.abs() - self.run_beg_speed) + 1.0).clamp(0.0, 1.0);
+            self.running_time = self.running_time.min(self.run_lerp_beg_time);
+        }
 
+        // Set a target velocity depending on your direction
+        let target_speed = || match is_key_down(KEY_RUN) {
+            false => self.walk_speed,
+            true  => {
+                // Lerp nicely between the beginning speed and the end speed (https://www.desmos.com/calculator/vl2lxkgnsv)
+                let t = (1.0 / (self.run_lerp_end_time - self.run_lerp_beg_time)) * self.running_time - (self.run_lerp_beg_time / (self.run_lerp_end_time - self.run_lerp_beg_time));
+                f32::lerp(self.run_beg_speed, self.run_end_speed, t.clamp(0.0, 1.0))
+            }
+        };
         self.target_x_vel = match self.walk_dir {
-            Some(WalkDir::Left)  => { self.flip_x = true; -target_speed },
-            Some(WalkDir::Right) => { self.flip_x = false; target_speed },
+            Some(WalkDir::Left)  => { self.flip_x = true; -target_speed() },
+            Some(WalkDir::Right) => { self.flip_x = false; target_speed() },
             None => 0.0,
         };
 
+        // TODO: vel_step
         // let vel_step = match grounded {
         //     true  => 300.0,
         //     false => 100.0,
@@ -150,27 +156,7 @@ impl Player {
         self.walk_timer = (self.walk_timer + self.vel.x.abs() / 1000.0).rem_euclid(1.0);
         if self.vel.x == 0.0 { self.walk_timer = 0.0; }
 
-        // Start skidding if you're going a direction opposed to your walk dir
-        // if matches!(self.walk_dir, Some(WalkDir::Right) if self.vel.x <= -self.target_x_vel)
-        // || matches!(self.walk_dir, Some(WalkDir::Left)  if self.vel.x >= -self.target_x_vel) {
-        //     println!("start skidding");
-        //     self.skid_start_speed = self.vel.x;
-        //     self.skidding = true;
-        // }
 
-        // Stop skidding if you're going the intended direction and you're above a certain velocity
-        // if self.skidding
-        // && (self.vel.x >= self.skid_start_speed && matches!(self.walk_dir, Some(WalkDir::Right))
-        // ||  self.vel.x <= self.skid_start_speed && matches!(self.walk_dir, Some(WalkDir::Left))
-        // || self.target_x_vel == 0.0) {
-        //     self.skidding = false;
-        // }
-
-        // println!("vel_x: {:?}\nskid_start_speed: {:?} target_x_vel: {:?}", self.vel.x, self.skid_start_speed, self.target_x_vel);
-    
-
-        // println!("{:?}", self.running_time);
-        
         self.pos += self.vel * delta;
 
         let grounded = self.grounded();
@@ -186,8 +172,8 @@ impl Player {
             (false, true,  ..) => 6, // Falling
             (.., true, _)    => 4, // Skidding
             (.., vel_x) if vel_x == 0.0 => 0, // Idle
-            (.., vel_x) if vel_x < 130.0 => if walk_frame {2} else {0} // Walking
-            _                            => if walk_frame {3} else {1} // Running
+            (.., vel_x) if vel_x < self.run_end_speed => if walk_frame {2} else {0} // Walking
+            _                                         => if walk_frame {3} else {1} // Running
         }
     }
 
